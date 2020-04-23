1e9512013fde5213f07ecd14db7afa502721e9a0
mayankrana835 <46513338+mayankrana835@users.noreply.github.com>
2020-04-23 12:39:10 +0530

    2D Project

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerBehaviour : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float movement = 0f;
+	public float movementSpeed = 10f;
+
+	
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+		movement = Input.GetAxis("Horizontal") * movementSpeed;
+	}
+
+	void FixedUpdate(){
+		rb.velocity = new Vector2(movement, rb.velocity.y);
+	}
+}
